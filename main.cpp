62c94f49541b8f79508c58ac76a02d2f2fa3e951
Anas Iqbal Lutfika <anasiqballutfika@gmail.com>
2026-02-12 13:06:31 +0700

    Menambahkan code nama

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <string>
 
 int main(){
     int angka;
+    std::string namaLengkap = "Anas Iqbal Lutfika";
 
     std::cout << "Masukkan angka: ";
     std::cin >>angka;
 
     if(angka == 1){
         std::cout << "Belajar membuat project" << std::endl;
+        std::cout << "Halo nama saya " << namaLengkap;
     }else{
         std::cout << "harusnya kamu memasukkan angka 1" << std::endl;
     }
