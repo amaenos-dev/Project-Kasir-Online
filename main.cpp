79334d9ae6ea96f72fa953b00ddf53f1ea4ffbe5
Anas Iqbal Lutfika <anasiqballutfika@gmail.com>
2026-02-12 15:21:53 +0700

    menambahkan pilihan menu dengan memasukkan input angka

--- main.cpp
+++ main.cpp
@@ -3,15 +3,21 @@
 
 int main(){
     int angka;
-    std::string namaLengkap = "Anas Iqbal Lutfika";
+    
+    std::cout << "Selamat Datang di Aplikasi Kasir" << std::endl << std::endl;
 
-    std::cout << "Masukkan angka: ";
+    std::cout << "1. Beli Produk" << std::endl;
+    std::cout << "2. Tambahkan Produk" << std::endl;
+    std::cout << "3. Lihat Keranjang" << std::endl;
+
+    std::cout << "Pilih Menu (Masukkan Angka): ";
     std::cin >>angka;
 
     if(angka == 1){
-        std::cout << "Belajar membuat project" << std::endl;
-        std::cout << "Halo nama saya " << namaLengkap;
-    }else{
-        std::cout << "harusnya kamu memasukkan angka 1" << std::endl;
+        std::cout << "Masuk ke menu Beli Produk" << std::endl;
+    }else if(angka == 2){
+        std::cout << "Masuk ke Menu Tambahkan Barang" << std::endl;
+    }else if(angka == 3){
+        std::cout << "Masuk ke Menu Lihat Keranjang" << std::endl;
     }
 }
