3349c771d077b16f27765c976965bc7959538a2e
Anas Iqbal Lutfika <anasiqballutfika@gmail.com>
2026-02-20 03:23:00 +0700

    update project dengan cmake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/_DISK/BELAJAR/C++/Project_Kasir_Online
+# Build directory: D:/_DISK/BELAJAR/C++/Project_Kasir_Online/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- main.cpp
+++ main.cpp
@@ -1,23 +1,6 @@
 #include <iostream>
-#include <string>
 
-int main(){
-    int angka;
-    
-    std::cout << "Selamat Datang di Aplikasi Kasir" << std::endl << std::endl;
-
-    std::cout << "1. Beli Produk" << std::endl;
-    std::cout << "2. Tambahkan Produk" << std::endl;
-    std::cout << "3. Lihat Keranjang" << std::endl;
-
-    std::cout << "Pilih Menu (Masukkan Angka): ";
-    std::cin >>angka;
-
-    if(angka == 1){
-        std::cout << "Masuk ke menu Beli Produk" << std::endl;
-    }else if(angka == 2){
-        std::cout << "Masuk ke Menu Tambahkan Barang" << std::endl;
-    }else if(angka == 3){
-        std::cout << "Masuk ke Menu Lihat Keranjang" << std::endl;
-    }    
+int main(int, char**){
+    std::cout << "Hello, from Project_Kasir_Online!\n";
+    return 0;
 }
